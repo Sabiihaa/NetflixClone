1f23cacf12f4ad68d9d630168db46145c8f5f5b6
Sabiihaa <167464800+Sabiihaa@users.noreply.github.com>
2025-06-08 20:46:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Navbar CSS propeerty in scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// profile section redirecting
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile-details.html";
+});
+
+// movie card
+const movieCard = document.querySelectorAll(".movie-card");
+movieCard.forEach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
